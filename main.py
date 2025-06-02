1ffe03a3c80dbfdc888cdfb5f583feb4ddef2adc
Lokesh G N <lokeshgn2.0@gmail.com>
2025-06-02 10:09:38 +0530

    Create main.py

--- main.py
+++ main.py
@@ -11,6 +11,12 @@ import shutil
 import cv2
 from collections import Counter
 
+processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large")
+model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large")
+
+tokenizer = AutoTokenizer.from_pretrained("google/flan-t5-large")
+model = AutoModelForSeq2SeqLM.from_pretrained("google/flan-t5-large")
+
 st.set_page_config(page_title="Video Captioning with SceneDetect", layout="centered")
 
 @st.cache_resource(show_spinner=False)
